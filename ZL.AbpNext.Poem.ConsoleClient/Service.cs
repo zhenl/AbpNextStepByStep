c7462f6f2ce4698a37d21ec5f72806ea2127675d
甄镭 <zhenlei1970@hotmail.com>
2021-11-23 20:18:46 +0800

    Revert "upgrade to .net 6"
    This reverts commit 56d45c010ca5841c3384e7c877ec9984e8448648.

--- ZL.AbpNext.Poem.ConsoleClient/Service.cs
+++ ZL.AbpNext.Poem.ConsoleClient/Service.cs
@@ -11,15 +11,31 @@ namespace ZL.AbpNext.Poem.ConsoleClient
 {
     public class Service : ITransientDependency
     {
-       
+        //IRepository<Poet> repository;
+        //IUnitOfWorkManager uowManager;
         IPoemAppService appService;
-        
+        //public Service(IRepository<Poet> repository, IUnitOfWorkManager uowManager)
+        //{
+        //    this.repository = repository;
+        //    this.uowManager = uowManager;
+        //}
         public Service(IPoemAppService appService)
         {
             this.appService = appService;
         }
         public void Run()
         {
+            //Console.WriteLine("你好");
+            //using (var uow = uowManager.Begin(new AbpUnitOfWorkOptions()))
+            //{
+            //    //获取第一个诗人
+            //    //var poet = repository.FirstOrDefault();
+            //    var poet = repository.AsQueryable().Include(p => p.Poems).FirstOrDefault();
+            //    Console.WriteLine(poet.Name);
+            //    Console.WriteLine(poet.Poems.Count());
+            //    Console.WriteLine(poet.Poems.ToList()[0].Author.Name);
+            //}
+
             var res=appService.GetPagedPoets(new Volo.Abp.Application.Dtos.PagedResultRequestDto { MaxResultCount = 10, SkipCount = 0 });
             Console.WriteLine(res.TotalCount);
             foreach(var dto in res.Items)
